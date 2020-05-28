7dfa788c0220ea5c738a0e2867d2320c7026cb44
elegantYu <845384559@qq.com>
2020-05-28 16:04:35 +0800

    feat: 添加icons

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -1,24 +1,24 @@
 /*
  * @Author: elegantYu
  * @Date: 2020-05-11 22:50:09
- * @LastEditTime: 2020-05-23 00:32:44
+ * @LastEditTime: 2020-05-28 15:57:17
  * @总有人要背锅，那为什么不能是我
  */
 const path = require("path");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const MergeJsonWebpackPlugin = require('merge-jsons-webpack-plugin')
-const templateList = require('./moreTemplate')
-const MergeLocale = require('./mergeLocale')
-const entryList = require('./entryList')
+const MergeJsonWebpackPlugin = require("merge-jsons-webpack-plugin");
+const templateList = require("./moreTemplate");
+const MergeLocale = require("./mergeLocale");
+const entryList = require("./entryList");
 
 module.exports = {
-  mode: 'production',
+  mode: "production",
   entry: entryList,
   output: {
     path: path.resolve(__dirname, "../dist"),
     filename: "static/js/[name].js",
-    chunkFilename: 'static/js/[name].bundle.js',
+    chunkFilename: "static/js/[name].bundle.js",
   },
   resolve: {
     extensions: [".js", ".jsx", ".json", ".scss", ".css"],
@@ -71,7 +71,7 @@ module.exports = {
       {
         test: /\.(sc|c)ss?$/,
         use: [
-          'style-loader',
+          "style-loader",
           "css-loader",
           {
             loader: "postcss-loader",
@@ -112,6 +112,11 @@ module.exports = {
       {
         from: "public/manifest.json",
         to: "manifest.json",
+        copyUnmodified: true,
+      },
+      {
+        from: "public/icons",
+        to: "static/icons",
         copyUnmodified: true,
       },
     ]),
